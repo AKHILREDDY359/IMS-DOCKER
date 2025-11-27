ac06392e776a3fb5dd7d507dd4a2ddb44af34b97
AKHILREDDY359 <159514831+AKHILREDDY359@users.noreply.github.com>
2025-11-28 00:59:37 +0530

    Add additional allowed origins for CORS

--- billingsoftware/src/main/java/in/inventory/billingsoftware/config/SecurityConfig.java
+++ billingsoftware/src/main/java/in/inventory/billingsoftware/config/SecurityConfig.java
@@ -57,7 +57,7 @@ public class SecurityConfig {
 
     private UrlBasedCorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of("http://localhost:5173","http://localhost:3000","http://localhost:80"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         config.setAllowCredentials(true);
